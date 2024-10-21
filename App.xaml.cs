81af79bbd460479859b7c87c4ef54be1f40de254
Jefferson <jeffersonruestapreciado@gmail.com>
2024-10-21 00:24:44 -0500

    Agregar archivo .gitignore para excluir archivos innecesarios

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppUgel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppUgel
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
