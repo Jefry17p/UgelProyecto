8aa4d7e2b7f1f60097027fe4894c6bcf2051dbf3
Jefferson Ruesta <jeffersonruestapreciado@gmail.com>
2024-11-17 12:01:31 -0500

    Los cambios necesarios para que muestre equipos

--- Models/EquiposCLS.cs
+++ Models/EquiposCLS.cs
@@ -9,23 +9,14 @@ namespace AppUgel.Models
     public class EquiposCLS
     {
         public int IdEquipo { get; set; }
-
-        public string NombreEqui { get; set; }
-
-        public string TipoEqui { get; set; }
-
-        public string SerieEqui { get; set; }
-
-        public string MarcaEqui { get; set; }
-
-        public string ModeloEqui { get; set; }
-
-        public string AreaEqui { get; set; } 
-
+        public string NombreEqui { get; set; } = null!;
+        public string TipoEqui { get; set; } = null!;
+        public string SerieEqui { get; set; } = null!;
+        public string MarcaEqui { get; set; } = null!;
+        public string ModeloEqui { get; set; } = null!;
+        public string AreaEqui { get; set; } = null!;
         public DateTime? FechaAdquisicion { get; set; }
-
-        public string EstadoEqui { get; set; }
-
+        public string EstadoEqui { get; set; } = null!;
         public DateTime FechaRegistro { get; set; }
     }
 }
